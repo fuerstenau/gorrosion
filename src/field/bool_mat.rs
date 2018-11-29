ebeaebea8c8c19b6ddb1184c4e8083309af71544
Peter Fürstenau <fuerstenau@mathematik.tu-darmstadt.de>
2018-11-29 19:41:24 +0100

    BoolVec stuff

--- src/field/bool_mat.rs
+++ src/field/bool_mat.rs
@@ -5,14 +5,27 @@ struct BoolMat {
 	contents: Vec<bool>,
 }
 
+struct BoolVec {
+	mat: BoolMat,
+}
+
 impl BoolMat {
-	fn new(height: usize, width: usize) -> BoolMat {
+	fn falses(height: usize, width: usize) -> BoolMat {
 		let contents = vec![false; height * width];
 		BoolMat {
 			height,
 			width,
 			contents,
 		}
+	}
+
+	fn from_diag(diag: BoolVec) -> BoolMat {
+		let len = diag.len();
+		let mut res = BoolMat::falses(len, len);
+		for i in diag.set_positions() {
+			res.set(i, i);
+		}
+		res
 	}
 
 	fn set(&mut self, i: usize, k: usize) {
@@ -26,8 +39,7 @@ impl BoolMat {
 	fn mult_get(A: &BoolMat, B: &BoolMat, i: usize, k: usize) -> bool {
 		assert_eq!(A.width, B.height);
 		let len = A.width;
-		(0..len).map(|n| A.get(i, n) & B.get(n, k))
-			.fold(false, |a, b| a | b)
+		(0..len).map(|n| A.get(i, n) & B.get(n, k)).any(|n| n)
 	}
 
 	fn mult(A: &BoolMat, B: &BoolMat) -> BoolMat {
@@ -48,13 +60,36 @@ impl BoolMat {
 	}
 }
 
+impl BoolVec {
+	fn falses(size: usize) -> BoolVec {
+		let mat = BoolMat::falses(size, 1);
+		BoolVec { mat }
+	}
+
+	fn len(&self) -> usize {
+		self.mat.height
+	}
+
+	fn get(&self, i: usize) -> bool {
+		self.mat.get(i, 1)
+	}
+
+	fn set_positions(&self) -> impl Iterator<Item = usize> {
+		let len = self.len();
+		let all_indices = 0..len;
+		let res: Vec<usize> =
+			all_indices.filter(|n| self.get(*n)).collect();
+		res.into_iter()
+	}
+}
+
 #[cfg(test)]
 mod tests {
 	#[test]
-	fn simple_mat() {
+	fn false_mat() {
 		use super::BoolMat;
 		assert_eq!(
-			BoolMat::new(3, 2),
+			BoolMat::falses(3, 2),
 			BoolMat {
 				height: 3,
 				width: 2,
@@ -66,7 +101,7 @@ mod tests {
 	#[test]
 	fn set() {
 		use super::BoolMat;
-		let mut bm = BoolMat::new(1, 3);
+		let mut bm = BoolMat::falses(1, 3);
 		bm.set(0, 1);
 		assert_eq!(
 			bm,
@@ -81,8 +116,8 @@ mod tests {
 	#[test]
 	fn mult() {
 		use super::BoolMat;
-		let mut A = BoolMat::new(1, 2);
-		let mut B = BoolMat::new(2, 3);
+		let mut A = BoolMat::falses(1, 2);
+		let mut B = BoolMat::falses(2, 3);
 		A.set(0, 0);
 		B.set(0, 1);
 		B.set(1, 0);
