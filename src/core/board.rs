f30c0654a8c6df606d034fb0c9f6010b0f043d59
Peter Fürstenau <fuerstenau@mathematik.tu-darmstadt.de>
2018-11-30 04:41:42 +0100

    Implement arbitrary graphs as boards

--- src/core/board.rs
+++ src/core/board.rs
@@ -5,6 +5,38 @@ pub trait Board {
 
 	fn index_to_num(&self, i: Self::Index) -> usize;
 	fn num_to_index(&self, n: usize) -> Self::Index;
-	fn adjacencies(&self) -> BoolMat;
+
+	fn size(&self) -> usize;
+	fn adjacencies(&self) -> &BoolMat;
+
 	fn is_hoshi(&self, i: Self::Index) -> bool;
+}
+
+pub struct Graph {
+	size: usize,
+	adj: BoolMat,
+}
+
+impl Board for Graph {
+	type Index = usize;
+
+	fn index_to_num(&self, n: Self::Index) -> usize {
+		n
+	}
+
+	fn num_to_index(&self, n: usize) -> Self::Index {
+		n
+	}
+
+	fn size(&self) -> usize {
+		self.size
+	}
+
+	fn adjacencies(&self) -> &BoolMat {
+		&self.adj
+	}
+
+	fn is_hoshi(&self, _i: Self::Index) -> bool {
+		false
+	}
 }
